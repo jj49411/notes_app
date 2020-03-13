7b847b5d2fd6d9b8bc643fe94687ed2f74180bd9
Julie <j49411@gmail.com>
2020-03-13 23:57:30 +0000

    feat: add a note and save in json database

--- app.js
+++ app.js
@@ -1,13 +1,60 @@
 const chalk = require('chalk');
 const yargs = require('yargs')
 
-const getNotes = require('./notes')
-
-const print = getNotes()
+const notes = require('./notes')
+// const print = getNotes()
 
 console.log(chalk.blue.bold('Success!'))
-console.log(process.argv)
-console.log(yargs.argv)
+// console.log(process.argv)
 
 
+//add, remove, read, list
  
+//create add command
+yargs.command({
+  command: 'add',
+  description: 'Add a new note',
+  builder: {
+    title: {
+      description: 'Note Title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      description: 'Note Body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function(argv) {
+    notes.addNotes(argv.title, argv.body)
+  }
+})
+//create remove command
+yargs.command({
+  command: 'remove',
+  description: 'Remove a note',
+  handler: function() {
+    console.log('removed!')
+  }
+})
+
+//create read command
+yargs.command({
+  command: 'read',
+  description: 'Read a note',
+  handler: function() {
+    console.log('read!')
+  }
+})
+
+//create list command
+yargs.command({
+  command: 'list',
+  description: 'List the note',
+  handler: function() {
+    console.log('listed!')
+  }
+})
+
+yargs.parse()
--- notes.js
+++ notes.js
@@ -1,5 +1,36 @@
+const fs = require('fs')
+
 const getNotes = function(){
   return 'Your notes...'
 }
+
+const addNotes = function(title, body) {
+  const notes = notesFile()
+
+  notes.push({
+    title: title,
+    body: body
+  })
+  saveNotes(notes)
+}
+
+const saveNotes = function(notes) {
+  const jsonObj = JSON.stringify(notes)
+  fs.writeFileSync('notes.json', jsonObj)
+}
+
+
+const notesFile = function() {
+  try{
+    const dateBuffer = fs.readFileSync('notes.json')
+    const dataJson = dateBuffer.toString()
+    return JSON.parse(dataJson)
+  } catch(e) {
+    return []
+  }
+}
   
-module.exports = getNotes
+module.exports = {
+  getNotes: getNotes,
+  addNotes: addNotes
+}
